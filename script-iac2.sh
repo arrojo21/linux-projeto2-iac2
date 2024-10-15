9539101e0b35030e5dbdfd97e3bfed0cd86ba4ca
arrojo21 <arrojo21@hotmail.com>
2024-10-15 21:57:26 +0000

    Apache v.1.0

---
+++ script-iac2.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+echo "Atualizando o servidor..."
+
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+echo "Baixando ee copiando os arquivos da aplicação..."
+
+cd /tmp
+wget https://github.com/arrojo21/linux-projeto1-iac/arquive/refs/heads/main.zip
+unzip main.zip
+cd linux-projeto1-iac
+cp -R * /var/www.html/
